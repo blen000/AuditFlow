8a39d51685bf021cc8281fd603aa2e73c9c8802c
B Zeg <bzeg2025@gmail.com>
2025-10-17 15:17:26 +0000

    no change yet after i update the rule in Fire store

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,3 @@
-export const firebaseConfig = {
-  "projectId": "studio-5859363143-60a07",
-  "appId": "1:2843569173:web:61534e4f3802ddbceb721c",
-  "apiKey": "AIzaSyB1EnUPmUUkFPri0NRKgjWTEiR_lG-Dg3w",
-  "authDomain": "studio-5859363143-60a07.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "2843569173"
-};
+// This file is intentionally left blank.
+// The Firebase configuration will be automatically provided by Firebase App Hosting.
+export const firebaseConfig = {};
